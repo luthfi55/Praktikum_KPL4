8d837d4b9de522afdae7b7b83bf979746870ea22
luthfi55 <luthfi12354@gmail.com>
2022-03-19 15:39:59 +0700

    first commit

---
+++ modul4_1302204013/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace modul4_1302204013
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
