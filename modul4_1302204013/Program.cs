9d28e77dbf615a325a7cd14bfb601c93bd28009a
luthfi55 <luthfi12354@gmail.com>
2022-03-19 16:05:02 +0700

    menambahkan class SimpleDataBase

--- modul4_1302204013/Program.cs
+++ modul4_1302204013/Program.cs
@@ -6,7 +6,24 @@ namespace modul4_1302204013
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Penjumlahan jumlah = new Penjumlahan();
+            Console.WriteLine("Masukkan angka pertama: ");
+            double x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Masukkan angka kedua: ");
+            double y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Masukkan angka ketiga: ");
+            double z = Convert.ToInt32(Console.ReadLine());
+            jumlah.JumlahTigaAngka(x,y,z);
+        }
+    }
+
+    public class Penjumlahan
+    {
+        private double data;
+        public void JumlahTigaAngka(double x, double y,double z)
+        {
+            data = x+y+z;
+            Console.WriteLine("\nHasil penjumlahan dari 3 angka tersebut adalah:\n"+data);
         }
     }
 }
